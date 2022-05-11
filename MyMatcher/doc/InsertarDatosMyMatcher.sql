7ea92db492d9ed1e168d299ce1fb688bebcf6e9e
MyMatcher <104069790+MyMatcher@users.noreply.github.com>
2022-05-11 12:53:35 +0200

    Add files via upload

---
+++ MyMatcher/doc/InsertarDatosMyMatcher.sql
@@ -0,0 +1,6 @@
+INSERT INTO `usuarios` (`User`, `Password`, `Nombre`, `Apellidos`, `Fechanac`, `Correo`, `Descripcion`, `Atributo1`, `Atributo2`, `Atributo3`, 
+`Atributo4`, `Atributo5`, `Atributo6`, `Atributo7`, `Atributo8`, `Atributo9`, `Atributo10`, `Atributo11`, `Atributo12`, `Atributo13`, `Atributo14`, `Atributo15`) VALUES
+('Alex23', '1234asd', 'Alex', 'Kovacs', '2000-12-12', 'alexkovacs27@gmail.com', 'aaaaaaaaaa', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 
+'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo'),
+('Alex29', '1234asd', 'Alex', 'Kovacs', '2000-12-12', 'alexkovacs27@gmail.com', 'aaaaaaaaaa', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 
+'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo', 'Feo');
